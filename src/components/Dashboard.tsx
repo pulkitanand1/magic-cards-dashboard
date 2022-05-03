6d415d6c349cd4c4f131864f41c94f9d52429e7c
Pulkit Anand <pulkit.anand@3pillarglobal.com>
2022-05-04 01:13:39 +0530

    Added cards to grid

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -20,30 +20,40 @@ export default function Dashboard() {
   }, [cards]);
 
   return (
-    <div className="container-xxl my-md-4 bd-layout">
-      <aside className="bd-sidebar">
-        <div className="bd-links">
-          <ul>
-            <li>hi</li>
-          </ul>
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-xl-2 col-md-3 mt-3">
+          <div className="card">
+            <div className="card-body">
+              <h5 className="class-title">{"Color"}</h5>
+              <div className="card-text row form-check">
+                <label>
+                  <input className="form-check-input" type="checkbox" /> black
+                </label>
+                <label>
+                  <input className="form-check-input" type="checkbox" /> white
+                </label>
+              </div>
+            </div>
+          </div>
+          <button>Apply</button>
         </div>
-      </aside>
-
-      <main className="bd-main order-1">
-        <div className="row">
-          {cards.map((card) => {
-            return (
-              <div key={card.id} className="col-sm-3">
-                <div className="card">
-                  <div className="card-body">
-                    <h6 className="class-title">{card.name}</h6>
+        <div className="col-xl-10 col-md-9">
+          <div className="row row-cols-1 row-cols-md-5 g-2 mt-1">
+            {cards.map((card) => {
+              return (
+                <div key={card.id} className="col">
+                  <div className="card">
+                    <div className="card-body">
+                      <h6 className="class-title">{card.name}</h6>
+                    </div>
                   </div>
                 </div>
-              </div>
-            );
-          })}
+              );
+            })}
+          </div>
         </div>
-      </main>
+      </div>
     </div>
   );
 }
